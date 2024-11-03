9660b2e82c4d21539cf73f2d589f732d3f384fd8
rgacorda <rgacorda.the2nd@gmail.com>
2024-11-03 20:20:30 +0800

    dd

--- src/app/(unauth)/property/company/[id]/page.tsx
+++ src/app/(unauth)/property/company/[id]/page.tsx
@@ -1,6 +1,6 @@
 import LessorBusinessProfileScreen from "@/modules/property/screens/LessorBusinessProfileScreen";
 
-export default async function inbox(companyId: Number) {
+export default async function inbox(companyId: number) {
     return (
         <div>
             <LessorBusinessProfileScreen companyId={companyId} />
